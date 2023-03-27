3ae3572c8094f46ef2e336ce4365451631f8dbe2
Alley Harmon <112970302+GOODSTROKE@users.noreply.github.com>
2023-03-26 23:20:59 -0400

    Update
    npm audit

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import BlogDetails from './pages/BlogDetails'
+import AddBlog from './pages/AddBlog'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import NotFoundPage from './pages/NotFoundPage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=''>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/details/:id' element={<BlogDetails />} />
+          <Route path='/add-blog' element={<AddBlog />} />
+          <Route path='*' element={<NotFoundPage />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
